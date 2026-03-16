037294e84df9f01355861eba165f44d14913da5b
Umesh I Ramawickrama <duiramawickrama@gmail.com>
2026-03-16 11:02:57 +0530

    newupdate

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate, useLocation, useNavigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate, useLocation, useNavigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import { SocketProvider } from './contexts/SocketContext';
